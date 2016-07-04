29b1ccdea8fec9bb8eb6d12314c233fee6574245
PamExx <thomas@pamexx.com>
2016-07-03 21:45:17 -0400

    initial commit

---
+++ gui/p2p_client.ml
@@ -0,0 +1,171 @@
+(* TODO: Detect a client disconnect *)
+
+let server_api = "http://localhost:8080/"
+
+open Lwt.Infix
+
+let (>>) = fun x y -> (x >>= fun _ -> y)
+
+type user = {
+  ip_address : string;
+  port : string;
+  username : string
+}
+
+let my_node : user ref = ref {
+  ip_address = "";
+  port = "";
+  username =""
+}
+
+let json_of_user u =
+  "  {\"ip_address\" : \"" ^ u.ip_address ^ "\", " ^
+  "\"port\" : \"" ^ u.port ^ "\", " ^
+  "\"username\" : \"" ^ u.username ^ "\"" ^
+  "}"
+
+(* Get request *)
+let get_it path =
+  let get_uri = Uri.of_string (server_api ^ path) in
+  Cohttp_lwt_unix.Client.get get_uri
+  >>= fun (a, b) -> b |> Cohttp_lwt_body.to_string
+  >|= fun s -> Yojson.Basic.from_string s
+
+(* Post request *)
+let post_it ~body path =
+  let post_uri = Uri.of_string (server_api ^ path) in
+  let post_header =
+    Cohttp.Header.init ()
+    |> fun h -> Cohttp.Header.add h "content-type" "application/json; charset=utf-8"
+  in
+  Cohttp_lwt_unix.Client.post ~headers:post_header ~body:(Cohttp_lwt_body.of_string body) post_uri
+  >>= fun (a, b) -> b |> Cohttp_lwt_body.to_string
+  >|= fun s -> Yojson.Basic.from_string s
+
+let user_of_json json_assoc =
+  let open Yojson.Basic.Util in
+  {
+    ip_address  = member "ip_address" json_assoc |> to_string;
+    port = member "port" json_assoc |> to_string;
+    username = member "username" json_assoc |> to_string
+  }
+
+(* Get a list of available nodes *)
+let get_all_nodes () =
+  let open Yojson.Basic.Util in
+  get_it "available_nodes"
+  >|= member "active_users"
+  >|= to_list
+  >|= List.map user_of_json
+
+(* Add the client to the list of nodes *)
+let register_node u =
+  post_it ~body:(json_of_user u) "add_node"
+
+(* TODO: Get a client socket set up, and get communication working between two clients *)
+(* Each client should always have an open socket for both reading and writing *)
+
+module ClientLwtServer = struct
+
+  open Lwt
+  open Unix
+  open Lwt_unix
+
+  let server_port = 12345
+  let so_timeout = Some 20
+  let backlog = 10
+
+  let try_close chan =
+    catch (fun () -> Lwt_io.close chan)
+    (function _ -> return ())
+
+  let init_socket sockaddr =
+    let socket = socket PF_INET SOCK_STREAM 0 in
+    setsockopt socket SO_REUSEADDR true;
+    bind socket sockaddr;
+    listen socket backlog;
+    socket
+
+  let process socket ~timeout ~callback =
+    let rec _process () =
+      Lwt_io.printl "Listening for a socket connection..." >>
+      accept socket >>=
+        (fun (socket_cli, _) ->
+          let inchan = Lwt_io.of_fd ~mode:Lwt_io.input socket_cli in
+          let outchan = Lwt_io.of_fd ~mode:Lwt_io.output socket_cli in
+          let c = callback inchan outchan in
+          let events =
+            match timeout with
+            | None -> [c]
+            | Some t -> [c; sleep (float_of_int t) >> return ()]
+          in
+          ignore (Lwt.pick events >> try_close outchan >> try_close inchan);
+          _process ()
+        )
+    in
+    _process ()
+
+  let launch_process () =
+    lwt server_addr = gethostbyname "localhost" in
+    let sockaddr = ADDR_INET (server_addr.h_addr_list.(0), server_port) in
+    let socket = init_socket sockaddr in
+    Lwt_main.run (
+      process
+      socket
+      ~timeout:so_timeout
+      ~callback:
+        (fun inchan outchan ->
+          Lwt_io.read_line inchan >>= (fun msg -> Lwt_io.printl msg))
+        (*fun inchan outchan ->
+          Lwt_io.read_line inchan >>= (fun msg -> Lwt_io.write_line outchan msg)*)
+    )
+
+end
+
+(* To test messages on localhost *)
+module ClientMsgSendLocalhost = struct
+
+  open Lwt_unix
+
+  lwt hentry = gethostbyname "localhost"
+
+  let send_msg msg =
+    let client_sock = socket PF_INET SOCK_STREAM 0 in
+    connect client_sock (ADDR_INET (hentry.h_addr_list.(0), ClientLwtServer.server_port))
+    >>= fun () -> send client_sock (Bytes.of_string msg) 0 (String.length msg) []
+    >>= fun _ -> close client_sock
+
+  let rec chat () =
+    Lwt_io.read_line Lwt_io.stdin
+    >>= send_msg
+    >>= chat
+
+end
+
+(* Communication between client nodes *)
+module ClientToClient = struct
+
+  open Lwt_unix
+
+  let send_msg ~ip ~port msg =
+    lwt hentry = gethostbyaddr @@ Unix.inet_addr_of_string ip in
+    let client_sock = socket PF_INET SOCK_STREAM 0 in
+    connect client_sock (ADDR_INET (hentry.h_addr_list.(0), int_of_string @@ port))
+    >>= fun () -> send client_sock (Bytes.of_string msg) 0 (String.length msg) []
+    >>= fun _ -> close client_sock
+
+end
+
+(* Send a message to all other nodes *)
+let send_to_all_nodes msg =
+  let open ClientToClient in
+  my_node := {
+    ip_address = "";
+    port = "1234";
+    username ="system76"
+  };
+  register_node !my_node >>
+  lwt all_nodes = get_all_nodes () in
+  Lwt_list.iter_p
+    (fun (u : user) -> send_msg ~ip:u.ip_address ~port:u.port ("Hello from " ^ u.username))
+    all_nodes
---
+++ p2p_compute.eliom
@@ -0,0 +1,159 @@
+{shared{
+  open Eliom_lib
+  open Eliom_content
+  open Html5.D
+
+  type user = {
+    ip_address : string;
+    port : string;
+    username : string
+  }
+
+}}
+
+{server{
+  let active_users : (string, user) Hashtbl.t = Hashtbl.create 100
+
+  let rec add_user u =
+    try (
+      if Hashtbl.mem active_users u.username
+      then (Hashtbl.remove active_users u.username; add_user u)
+      else Hashtbl.add active_users u.username u
+    )
+    with
+    | Not_found -> Hashtbl.add active_users u.username u
+
+  let remove_user u = Hashtbl.remove active_users u.username
+
+  let get_all_users () = Hashtbl.fold (fun k v acc -> v :: acc) active_users []
+
+  let json_of_user u =
+    "  {\"ip_address\" : \"" ^ u.ip_address ^ "\", " ^
+    "\"port\" : \"" ^ u.port ^ "\", " ^
+    "\"username\" : \"" ^ u.username ^ "\"" ^
+    "}"
+
+  let json_of_users users =
+    let users_list = List.map (json_of_user) (get_all_users ()) in
+    "{\"active_users\" : [\n" ^ (String.concat ",\n" users_list) ^ "\n]}"
+
+  let user_of_json json_assoc =
+    let open Yojson.Basic.Util in
+    {
+      ip_address  = member "ip_address" json_assoc |> to_string;
+      port = member "port" json_assoc |> to_string;
+      username = member "username" json_assoc |> to_string
+    }
+
+  (* Add some fake users for testing *)
+  let () = add_user {ip_address ="123.32.5.7"; port = "1234"; username = "test_user_1"}
+  let () = add_user {ip_address ="124.31.5.7"; port = "5678"; username = "test_user_2"}
+  let () = add_user {ip_address ="126.62.0.7"; port = "1234"; username = "test_user_3"}
+  let () = add_user {ip_address ="127.37.5.1"; port = "5555"; username = "test_user_4"}
+  let () = add_user {ip_address ="129.32.8.9"; port = "1123"; username = "test_user_5"}
+
+}}
+
+module P2p_compute_app =
+  Eliom_registration.App (
+    struct
+      let application_name = "p2p_compute"
+    end)
+
+let main_service =
+  Eliom_service.App.service ~path:[] ~get_params:Eliom_parameter.unit ()
+
+let
+  () =
+  P2p_compute_app.register
+    ~service:main_service
+    (fun () () ->
+      let ip = Eliom_request_info.get_remote_ip () in
+      Lwt.return
+        (Eliom_tools.F.html
+           ~title:"p2p_compute"
+           ~css:[["css";"p2p_compute.css"]]
+           Html5.F.(body [
+             h2 [pcdata "Welcome from Eliom's distillery!"];
+             h2 [pcdata ("Your ip address is: " ^ ip)];
+           ])))
+
+let available_nodes_service =
+  Eliom_service.Http.service ~path:["available_nodes"] ~get_params:Eliom_parameter.unit ()
+
+let add_node_fallback =
+  Eliom_service.App.service
+    ~path:["add_node"]
+    ~get_params:Eliom_parameter.unit
+    ()
+
+let
+  () =
+  P2p_compute_app.register
+    ~service:add_node_fallback
+    (fun () () ->
+      Lwt.return
+        (Eliom_tools.F.html
+           ~title:"p2p_compute"
+           ~css:[["css";"p2p_compute.css"]]
+           Html5.F.(
+             body
+             [h2
+              [pcdata "add_node fallback page"];
+             ]
+           )
+        )
+    )
+
+let add_node_service =
+  Eliom_service.Http.post_service
+    ~fallback:add_node_fallback
+    ~post_params:Eliom_parameter.raw_post_data
+    ()
+
+let json_mime_type = "application/json"
+
+let send_json ~code json =
+  Eliom_registration.String.send ~code (json, json_mime_type)
+
+let check_content_type ~mime_type content_type =
+  match content_type with
+  | Some ((type_, subtype), _) when (type_ ^ "/" ^ subtype) = mime_type -> true
+  | _ -> false
+
+let read_raw_content ?(length = 4096) raw_content =
+  let content_stream = Ocsigen_stream.get raw_content in
+  Ocsigen_stream.string_of_stream length content_stream
+
+let available_nodes_handler () () =
+  send_json ~code:200 (json_of_users @@ get_all_users ())
+
+let add_node user_ip (content_type, raw_content_opt) =
+  if not (check_content_type ~mime_type:json_mime_type content_type)
+  then send_json ~code:400 "{\"error\" : \"Content type is wrong, it must be JSON.\"}"
+  else
+    match raw_content_opt with
+    | None -> send_json ~code:400 "{\"error\" : \"Body content is missing.\"}"
+    | Some raw_content ->
+      read_raw_content raw_content
+      >>= fun s ->
+      try (
+        let json = Yojson.Basic.from_string s in
+        let u = user_of_json json in
+        add_user {
+          ip_address = user_ip; (* Override the users sent ip_address with what the server sees *)
+          port = u.port;
+          username = u.username
+        };
+        send_json ~code:200 "{\"accepted\" : true}"
+      )
+      with
+      | _ -> send_json ~code:200 "Fail"
+
+let add_node_handler () content =
+  let ip = Eliom_request_info.get_remote_ip () in
+  add_node ip content
+
+let () =
+  Eliom_registration.Any.register available_nodes_service available_nodes_handler;
+  Eliom_registration.Any.register add_node_service add_node_handler
